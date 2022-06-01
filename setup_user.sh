a31a4badd937c678d87dbfccd61cadcb7716cabe
Jay Leimer <dev@accellagroup.com>
2022-06-01 09:56:32 +0200

    Fixed Config"

--- setup_user.sh
+++ setup_user.sh
@@ -1,3 +1,5 @@
 sudo su -
 useradd -m -d /home/$1 -s /bin/bash $1
-echo -e "$2\n$2" | passwd $1'
+echo -e "$2\n$2" | passwd $1
+cp -pr /home/vagrant/.ssh /home/$1/.ssh
+echo -e "%$1 ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$1
